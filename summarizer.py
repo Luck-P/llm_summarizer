fd646a98ca2c966724624bcca57fa7022634d423
Luck-P <luck78240@gmail.com>
2025-08-01 21:05:10 +0200

    0.1.2 : smarter llm choice - issue > follow-up questions quickly saturate llm context capacity despite removing document summary from %history%

--- summarizer.py
+++ summarizer.py
@@ -19,29 +19,53 @@ load_dotenv()
 
 print(f"checkup : \nOPENAI_API_KEY : {os.environ["OPENAI_API_KEY"]}\nTAVILY_API_KEY : {os.environ["TAVILY_API_KEY"]}");input("...type...")
 
-querytl = TavilySearchResults()
+#querytl = TavilySearchResults()
 
-filetl = 1
+###     MODELS      ###
+'''
+openrouter.ai : many models available with own pros n cons
+
+    document summarization  > high throughput - latency less relevant - large context - tuned for better document processing
+
+    follow-up questions     > low latency - throughput / context less relevant - engineering studies : good-at-maths model 
+
+'''
 
 qwen1 = ChatOpenAI(
     openai_api_base= "https://openrouter.ai/api/v1",
     model_name = "qwen/qwen3-coder:free"
-)
+)#said to be fine-tuned for coding 
+
+llama32 = ChatOpenAI(
+    openai_api_base="https://openrouter.ai/api/v1",
+    model_name="meta-llama/llama-3.2-3b-instruct:free"
+) #low latency (0.5s) - high throughput (223.5 t/s) - large context (131,000 tk) - good at advanced natural language processing  
+
+dpskllama = ChatOpenAI(
+    openai_api_base="https://openrouter.ai/api/v1",
+    model_name="deepseek/deepseek-r1-distill-llama-70b:free"
+) #low latency (0.85) - decent throughput (52 t/s) - small context (8,192 tk) - good at maths  
+
+###     TOOLS       ###
 
 def docsummar(path):
     return '\n'.join(str(el) for el in partition(filename = path))[:4000]
     
-'''dosum = Tool(
+dosum = Tool(
     name="document summarizer",
     func = docsummar,
     description = "retrieve the document's text - input should be the full file path provided by the user"
 )
 
+###     AGENTS      ### 
+
 synt = initialize_agent(
-    llm = qwen1,
+    llm = llama32,
     agent = AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
     tools=[dosum]
-)'''
+)
+
+###     LOW FUNCTIONS       ###
 
 def getpath():
     path = input("give file's path (windows format): ")
@@ -51,21 +75,28 @@ def getpath():
         print("incorrect input")
         return getpath() 
         
+def clstdout():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 if __name__=="__main__":
     print("document summeriser")
     path = getpath()
     print(path)
+    print(docsummar(path))
     history=[SystemMessage(content=f"summarize the following document. Your output should consist in : a quick overview (type, theme) - a light resume breaking the main points down - a short conclusion ||| document : {docsummar(path)}")]
-    res = qwen1.invoke(history)
+    res = llama32.invoke(history)
     history.append(res)
+    clstdout()
     print(res.content)
-    fwup = input("'q' to quit > ")
+    fwup = input("\n'q' to quit > ")
     while(fwup!='q'):
         history.append(HumanMessage(content=fwup))
-        res = qwen1.invoke(history);history.append(res);print(res.content)
-        fwup = input("'q' to quit > ")
+        res = dpskllama.invoke(history)
+        #history.append(res); > too big prompt -> counterproductive, llm answer comes from document anyway
+        print(f'\n{res.content}')
+        fwup = input("\n'q' to quit > ")
     print("over")
+    clstdout()
     
 
 
